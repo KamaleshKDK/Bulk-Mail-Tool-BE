995c5cf7623badebc5e8d52476077fd8a97245ac
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-12 23:59:23 +0530

    mongo added

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ let nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const creds = require("./credential.json");
 const cors = require("cors");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const URL ="mongodb+srv://BulkMail:bulkmail@cluster0.pazhx.mongodb.net?retryWrites=true&w=majority"
+// const URL = "mongodb://localhost:27017";
+
 
 //Initiate Express and CORS and Body Parser
 
@@ -23,15 +28,24 @@ let transporter = nodemailer.createTransport({
     },
 });
 
-// api for sending emails
+// API for sending emails
 
-app.post("/mail", (req, res, next) => {
+app.post("/mail", async (req, res) => {
+    try {
+        let connection = await MongoClient.connect(URL);
+        let db = connection.db("Bulk_Mail_Tool");
+        await db.collection("Mails").insertOne(req.body);
+        connection.close();
+        res.json({ message: "Mail Added" })
+    } catch (error) {
+        console.log(error)
+    }
     console.log(req.body.email);
     var email = req.body.email
     var message = req.body.message
     var subject = req.body.subject
     var name = req.body.name
-  
+
     const mailOptions = {
         from: name,
         to: email,
