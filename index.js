42d9da7df25fd87ea1d859550953f3293615abd7
kamaleshKDK <kamaleshkdk@gmail.com>
2022-03-22 22:59:03 +0530

    login attached

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const connection = require("./db");
+const userRoutes = require("./routes/user");
+const authRoutes = require("./routes/auth");
 let nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
-const cors = require("cors");
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const URL =process.env.URL;
-// const URL = "mongodb://localhost:27017";
 
 
-//Initiate Express and CORS and Body Parser
+//database
+connection();
 
-let app = express();
+//MiddleWares
+
+app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+//Routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
 
 // Nodeamailer
 
@@ -31,7 +41,7 @@ let transporter = nodemailer.createTransport({
 
 app.post("/mail", async (req, res) => {
     try {
-        let connection = await MongoClient.connect(URL);
+        let connection = await MongoClient.connect(process.env.DB);
         let db = connection.db("Bulk_Mail_Tool");
         await db.collection("Mails").insertOne(req.body);
         connection.close();
@@ -77,7 +87,5 @@ transporter.verify(function (err, success) {
     }
 })
 
-// PORT Listening
-
-const PORT = process.env.PORT || 3030
-app.listen(PORT, () => console.info("Server has Started", PORT))
+const port = process.env.PORT || 3030;
+app.listen(port, () => console.log(`Listening on port ${port}`));
